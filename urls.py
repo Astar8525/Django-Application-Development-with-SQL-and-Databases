e1bdd4e56cef9422b2c12e7b72626c9af057a99f
Anthony Norman <anthonynorman85@gmail.com>
2025-07-27 12:29:56 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('course-details/', views.course_details, name='course_details'),
-    path('mock-exam-result/', views.mock_exam_result, name='mock_exam_result'),
+    path('course/', views.course_details, name='course_details'),
 ]
+
