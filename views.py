fd87aa8e03c182afc3f6251808341d183f5f5d9b
Anthony Norman <anthonynorman85@gmail.com>
2025-07-27 12:29:22 -0500

    Update views.py

--- views.py
+++ views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from .models import Question, Choice, Submission
+from .models import Question
 
 def course_details(request):
     questions = Question.objects.all()
     return render(request, 'course_details_bootstrap.html', {'questions': questions})
 
-def mock_exam_result(request):
-    # Dummy result for screenshot
-    score = 85
-    return render(request, 'mock_exam_result.html', {'score': score})
